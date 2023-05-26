ceef4038ef6f3573802c664ba880f4f9c686a212
Simon <simon.louis638@gmail.com>
2023-05-26 09:14:03 +0200

    method unittime for different status

--- tamagotchi/src/main/java/com/ynov/Tamagotchi.java
+++ tamagotchi/src/main/java/com/ynov/Tamagotchi.java
@@ -7,11 +7,57 @@ public class Tamagotchi {
     String state;
     private int nbPlay = 0;
     private int nbEat = 0;
+    private boolean isSick;
 
 public Tamagotchi () {
     age = 0;
     happiness = 15;
     happinessMax = 50;
     state = "Oeuf";
+    this.isSick = false;
+    }
+
+    public void UnitTime() {
+        age++;
+        nbPlay = 0;
+        if (state == "Oeuf") {
+            happiness = 15;
+            if (age >= 1) {
+                state = "Bébé";
+                System.out.println("Le Tamagotchi est né !");
+            }
+        } else if (state == "Bébé") {
+            happiness -= 10;
+            if (age >= 4 && happiness >= 40) {
+                state = "Adulte";
+                System.out.println("Le Tamagotchi est devenu adulte !");
+            }
+        } else if (state == "Adulte") {
+            happiness -= 10;
+            if (age >= 15) {
+                state = "Vieillard";
+                System.out.println("Le Tamagotchi est devenu vieux !");
+            }
+        } else if (state == "Vieillard") {
+            happiness -= 10;
+            if (isSick == Math.random() < 1 / 3) {
+                isSick = true;
+                System.out.println("Le Tamagotchi est tombé malade !");
+            }
+            if (isSick == true) {
+                age = 0;
+                happiness = 15;
+                state = "Oeuf";
+                System.out.println("Le Tamagotchi est mort de malade.");
+            }
+            }
+            if (age >= 20) {
+                age = 0;
+                happiness = 15;
+                state = "Oeuf";
+                System.out.println("Le Tamagotchi est mort de vieillesse.");
+            }
     }
 }
+
+
